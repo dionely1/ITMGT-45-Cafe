1bfb1070dec195401e2f7fc68bafd617c3430ba9
dionely1 <98880013+dionely1@users.noreply.github.com>
2022-06-01 21:14:53 +0800

    Add files via upload

---
+++ ITMGT 45 Assignnments/assignment0_ITM45_192138.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+'''Assignment 0
+This assignment is a giveaway assignment.
+By completing this assignment, you will familiarize yourself with this
+    course's submission mechanics.
+'''
+
+def three_number_average(x, y, z):
+    '''Item 1.
+    Three Number Average. 3 points.
+    Returns the average of three numbers.
+    Parameters
+    ----------
+    x: int
+        the first number
+    y: int
+        the second number
+    z: int
+        the third number
+    Returns
+    -------
+    float
+        the average of x, y, and z
+    '''
+    # Write your code below this line
+
+
+# In[ ]:
+
+
+print("Hi! This code returns the average of three numbers")
+x=int(input("First Number"))
+y=int(input("Second Number"))
+z=int(input("Third Number"))
+average=int((x+y+z)/3)
+print("The average of x,y,z is", average)
+
+
+# In[ ]:
+
+
+
+
